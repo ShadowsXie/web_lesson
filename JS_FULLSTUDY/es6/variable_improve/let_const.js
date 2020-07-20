d54b92a8c6f35e5ef6ad65e9d991f3053c0032ef
ShadowsXie <1084827833@qq.com>
2020-07-20 18:08:19 +0800

    js学习

---
+++ JS_FULLSTUDY/es6/variable_improve/let_const.js
@@ -0,0 +1,49 @@
+// var name 
+// var obj = {}
+
+// console.log(name)
+
+// var name = "张三"
+
+// name = "张三"
+
+// console.log(name)
+// let name = "皮皮"
+
+// for(let i = 0; i < 10; i++){
+//     console.log(i)
+// }
+// console.log(i)
+
+
+// var a =[]
+
+// for (let i = 0;i < 10; i++) {
+//     a[i] = function(){
+//         console.log(i)
+//     }
+// }
+// a[6]()
+
+
+// for (let i = 0; i < 3; i++) {//父作用域
+//     let i = 'abc' // 子作用域
+//     console.log(i)
+// }
+
+
+//暂时性死区
+// var tmp = 123
+
+// if (true) {
+//     tmp = 'abc'
+//     let tmp 
+// }
+
+var a = 'a'
+var a = 'aa'
+console.log(a)
+
+let b = 'b'
+let b = 'bb'
+console.log(b)
