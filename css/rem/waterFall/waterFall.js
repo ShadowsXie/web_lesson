3c4d7fbf535ce99c7cc3e114cbfce8cfdbc04206
ShadowsXie <1084827833@qq.com>
2020-07-27 21:42:58 +0800

    瀑布流

--- css/rem/waterFall/waterFall.js
+++ css/rem/waterFall/waterFall.js
@@ -1,8 +1,53 @@
 window.onload = function() {
-
+    imgLocation("container", "box")
 }
 
 //获取当前有多少张图片要摆放
 function imgLocation(parent, content) {
-    document.getElementById
+    // 将parent下的所有内容全部取出
+    let cparent = document.getElementById(parent)
+    // console.log(cparent)
+    let ccontent = getChildElement(cparent,content)
+    //console.log(ccontent)
+    let imgWidth = ccontent[0].offsetWidth
+    let num = Math.floor(document.documentElement.clientWidth/imgWidth)
+    cparent.style.cssText = "width:" + imgWidth * num + "px"
+
+    let BoxHeightArr = []
+    for (let i = 0; i < ccontent.length; i++) {
+        if(i<num){
+            BoxHeightArr[i] = ccontent[i].offsetHeight
+            // console.log(BoxHeightArr[i])
+        } else {
+            // 将Math.min方法借给数组用
+            let minHeight = Math.min.apply(null,  BoxHeightArr)
+            let minIndex = getminHeightLocation(BoxHeightArr,minHeight)
+            // console.log(minHeight)
+            ccontent[i].style.position = 'absolute'
+            ccontent[i].style.top = minHeight + 'px'
+            ccontent[i].style.left = ccontent[minIndex].offsetLeft + 'px'
+            BoxHeightArr[minIndex] = BoxHeightArr[minIndex] + ccontent[i].offsetHeight
+        }
+    }
+}
+
+
+function getChildElement(parent, content) {
+    let contentArr = []
+    let allCount = parent.getElementsByTagName('*')
+    // console.log(allCount)
+    for (let i = 0; i <allCount.length; i++) {
+        if(allCount[i].className === content){
+            contentArr.push(allCount[i])
+        }
+    } 
+    return contentArr
+}
+
+function getminHeightLocation(BoxHeightArr,minHeight){
+    for(let i in BoxHeightArr) {
+        if(BoxHeightArr[i] === minHeight) {
+            return i
+        }
+    }
 }
