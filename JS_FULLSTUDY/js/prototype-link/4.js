52da8888770fa7c129052d1883683718cc6a209b
ShadowsXie <1084827833@qq.com>
2020-08-23 14:45:21 +0800

    prototype-link

--- JS_FULLSTUDY/js/prototype-link/4.js
+++ JS_FULLSTUDY/js/prototype-link/4.js
@@ -1,2 +1,17 @@
 var num = 123
 num.toString()    // '123'    ----> new Number(num).toString()
+
+
+
+
+
+// 方法重写
+Object.prototype.toString = function() {
+    return 'hahaha'
+}
+
+function Person() {
+
+}
+var person = new Person()
+console.log(person.toString())
