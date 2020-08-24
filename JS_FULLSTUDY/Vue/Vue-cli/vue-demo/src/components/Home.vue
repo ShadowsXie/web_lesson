578fad8bd4e57416861071b196e25ab8ae35a7bb
ShadowsXie <1084827833@qq.com>
2020-08-24 16:59:02 +0800

    Vue-cli

---
+++ JS_FULLSTUDY/Vue/Vue-cli/vue-demo/src/components/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+      <p class="name">蜗牛</p>
+      <p class="age">18</p>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
