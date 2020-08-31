fc39372d6f6990e64ce76d947d2189f5b87e066a
ShadowsXie <1084827833@qq.com>
2020-08-31 12:08:40 +0800

    vue-elementui

---
+++ JS_FULLSTUDY/Vue/vue-elementui/src/views/Login.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="login">
+      <h2>旅梦后台管理系统</h2>
+      <div class="login-wrapper">
+          <p class="input">
+              <el-input v-model="user" placeholder="请输入账号"></el-input>
+          </p>
+          <p class="input">
+               <el-input v-model="password" placeholder="请输入密码"></el-input>
+          </p>
+          <p class="btn">
+              <el-button type="primary" round @click="login">登录</el-button>
+          </p>        
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+        user: '',
+        password: ''
+      }
+    },
+    methods: {
+        login() {
+            if (!this.user || !this.password) {
+                this.$message.error('账号或密码不能为空');
+            }else if(this.user === 'admin' && this.password === '123456') {
+                this.$message({
+                    showClose: true,
+                    message: '登录成功',
+                    type: 'success'
+                });
+                // 路由跳转
+                this.$router.push({ path: '/home'})
+            } else {
+                 this.$message.error('账号或密码错误！');
+            }
+        }
+    }
+}
+</script>
+    
+<style scoped>
+    .login{
+        position: absolute;
+        top: 0;
+        right: 0;
+        left: 0;
+        bottom: 0;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-flow: column;
+        background: aquamarine;
+    }
+    .login-wrapper{
+        margin-top: 30px;
+        width: 250px;
+
+    }
+    .input{
+        margin-bottom: 20px;
+    }
+    .btn button{
+        width: 100%;
+    }
+</style>
