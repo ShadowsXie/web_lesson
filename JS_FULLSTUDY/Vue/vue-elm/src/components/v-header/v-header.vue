b8d5d4d6a8a4f3cedf53bd81d122fcc4626bd0cf
ShadowsXie <1084827833@qq.com>
2020-09-01 12:14:57 +0800

    vue-elm

---
+++ JS_FULLSTUDY/Vue/vue-elm/src/components/v-header/v-header.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="header">
+    <div class="content-wrapper">
+      <div class="avatar">
+        <img
+          width="64"
+          height="64"
+          src="https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=222120700,1745793916&fm=26&gp=0.jpg"
+          alt
+        />
+      </div>
+      <div class="content">
+        <div class="title">
+          <span class="brand"></span>
+          <span class="name">粥品香坊（回龙观）</span>
+        </div>
+        <div class="description">蜂鸟专送/38分钟送达</div>
+        <div class="support">
+          <img src="./brand@2x.png" alt />
+          <span class="text">在线支付满28减29</span>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+
+</style>
