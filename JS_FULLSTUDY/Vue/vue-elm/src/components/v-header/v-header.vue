c85353e50e5da7d3356a282bc09542f529a19872
ShadowsXie <1084827833@qq.com>
2020-09-02 17:24:31 +0800

    vue-elm

--- JS_FULLSTUDY/Vue/vue-elm/src/components/v-header/v-header.vue
+++ JS_FULLSTUDY/Vue/vue-elm/src/components/v-header/v-header.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="header">
+  <div class="header" @click="showDetail">
     <div class="content-wrapper">
       <div class="avator">
         <img
@@ -20,12 +20,27 @@
           <span class="text">{{sellers.supports[0].description}}</span>
         </div>
       </div>
+      <div class="support-count" v-if="sellers.supports">
+        <span class="count">{{sellers.supports.length}}个</span>
+        <i class="icon-keyboard_arrow_right"></i>
+      </div>
     </div>
+    <div class="bulletin-wrapper">
+      <span class="bulletin-title"></span>
+      <span class="bulletin-text">{{sellers.bulletin}}</span>
+      <i class="icon-keyboard_arrow_right"></i>
+    </div>
+    <div class="background">
+      <img :src="sellers.avatar" alt="" width="100%" height="100%">
+    </div>
+    <!-- detail -->
+    <header-detail :seller="sellers" v-show="detailVisible" @hide="hideDetail"></header-detail>
   </div>
 </template>
 
 <script>
 import SupportIco from '@/components/support-ico/support-ico'
+import HeaderDetail from '@/components/header-detail/header-detail'
 export default {
   props: {
     seller: {
@@ -37,7 +52,8 @@ export default {
   },
   data() {
     return {
-      sellers: {}
+      sellers: {},
+      detailVisible: false
     }
   },
   created() {
@@ -46,8 +62,17 @@ export default {
       this.sellers = this.seller
     }, 1000)  
   },
+  methods: {
+    showDetail() {
+      this.detailVisible = true
+    },
+    hideDetail(e) {
+      this.detailVisible = e
+    }
+  },
   components: {
-    SupportIco
+    SupportIco,
+    HeaderDetail
   }
 };
 </script>
@@ -99,4 +124,56 @@ export default {
         .text
           line-height 12px
           font-size $fontsize-small-s
+    .support-count
+      position absolute
+      right 12px
+      bottom 14px
+      display flex
+      align-items center
+      padding 0 8px
+      height 24px
+      line-height 24px
+      text-align center
+      border-radius 14px
+      background $color-background-sss
+      .count
+        font-size $fontsize-small-s
+      .icon-keyboard_arrow_right
+        margin-left 2px
+        line-height 24px
+        font-size $fontsize-small-s
+  .bulletin-wrapper
+    position relative
+    display flex
+    align-items center
+    height 28px
+    line-height 28px
+    padding 0 8px
+    background $color-background-sss
+    .bulletin-title
+      flex 0 0 22px
+      width 22px
+      height 12px
+      margin-right 4px
+      bg-image('bulletin')
+      background-size 22px 12px
+      background-repeat no-repeat
+    .bulletin-text
+      flex 1
+      white-space nowrap
+      overflow hidden 
+      text-overflow ellipsis
+      font-size $fontsize-small-s
+    .icon-keyboard_arrow_right
+      flex 0 0 10px
+      width 10px
+      font-size $fontsize-small-s
+  .background
+    position absolute
+    top 0
+    left 0
+    width 100%
+    height 100%
+    z-index -1
+    filter blur(10px)
 </style>
