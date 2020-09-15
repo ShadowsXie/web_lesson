69252da53885aef738e5be87707748c523707218
ShadowsXie <1084827833@qq.com>
2020-09-15 20:51:08 +0800

    vue-notebook

---
+++ JS_FULLSTUDY/Vue/vue-notebook/front/src/router/index.js
@@ -0,0 +1,39 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'StarNotes',
+    component: () => import('@/views/StarNotes.vue'),
+    meta: {
+      title: '星辰笔记'
+    }
+  },
+  {
+    path: '/StarBanner',
+    name: 'StarBanner',
+    component: () => import('@/views/StarBanner.vue'),
+    meta: {
+      title: '欢迎'
+    }
+  },
+  {
+    path: '/StarLogin',
+    name: 'StarLogin',
+    component: () => import('@/views/StarLogin.vue'),
+    meta: {
+      title: '登录'
+    }
+  }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
