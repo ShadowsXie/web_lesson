bc5ce1bd6c1e17166adda99a837b9f5dfc25bb13
ShadowsXie <1084827833@qq.com>
2020-09-16 23:24:52 +0800

    vue-notebook

---
+++ JS_FULLSTUDY/Vue/vue-notebook/note-book/controllers/mySqlConfig.js
@@ -0,0 +1,65 @@
+var mysql = require('mysql')
+var config = require('./defaultConfig')
+
+// 连接线程池
+var pool = mysql.createPool({
+  host: config.database.HOST,
+  user: config.database.USENAME,
+  password: config.database.PASSWORD,
+  database: config.database.DATABASE,
+  port: config.database.PORT,
+})
+
+// 统一连接数据库的方法
+let allServices = {
+  query: function(sql, values) {
+    return new Promise((resolve, reject) => {
+      pool.getConnection(function(err, connection) {   // 建立连接
+        if (err) {
+          reject(err)
+        } else {
+          connection.query(sql, values, (err, rows) => {
+            if (err) {
+              reject(err)
+            } else {
+              resolve(rows)
+            }
+            connection.release()   // 释放链接
+          })
+        }
+      })
+    })
+  }
+}
+
+// 读取所有users表数据
+let getAllUsers = function() {
+  let _sql = `select * from users;`
+  return allServices.query(_sql)
+}
+
+// 用户登录
+let userLogin = function(username, userpwd) {
+  let _sql = `select * from users where username="${username}" and userpwd="${userpwd}";`
+  return allServices.query(_sql)
+}
+
+// 查找用户
+let findUser = function(username) {
+  let _sql = `select * from users where username="${username}";`
+  return allServices.query(_sql)
+}
+
+// 用户注册
+let insertUser = function(value) {
+  let _sql = `insert into users set username=?,userpwd=?,nickname=?;`
+  return allServices.query(_sql, value)
+}
+
+// 导出方法
+module.exports = {
+  getAllUsers,
+  userLogin,
+  findUser,
+  insertUser
+}
