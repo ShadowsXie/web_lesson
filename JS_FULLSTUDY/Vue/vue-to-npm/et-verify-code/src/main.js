6c8bd76b9e6bb58db88a5518aaac4b07aa0de757
ShadowsXie <1084827833@qq.com>
2020-10-08 15:17:44 +0800

    vue-to-npm

---
+++ JS_FULLSTUDY/Vue/vue-to-npm/et-verify-code/src/main.js
@@ -0,0 +1,3 @@
+import EtVerifyCode from './et-verify-code.vue'
+EtVerifyCode.install = Vue => Vue.component(EtVerifyCode.name, EtVerifyCode)
+export default EtVerifyCode
---
+++ JS_FULLSTUDY/Vue/vue-to-npm/et-verify-code/vue.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  css: {
+    extract: false  // 组件的样式是否另外打包成单独的CSS文档
+  },
+  productionSourceMap:false
+}
