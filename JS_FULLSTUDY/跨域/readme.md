0d1ea2424d4d9ee3a8fc2f10c7eaba37b681b302
ShadowsXie <1084827833@qq.com>
2020-10-12 20:49:14 +0800

    跨域

---
+++ JS_FULLSTUDY/跨域/readme.md
@@ -0,0 +1,9 @@
+浏览器的安全策略
+
+端口 域名 协议： 不同 就有跨域
+baidu.com  api.baidu.com
+
+## 
+cors
+## jsonP
+## 反向代理
---
+++ JS_FULLSTUDY/跨域/server/index.js
@@ -0,0 +1,20 @@
+const http = require('http')
+const qs = require('querystring')
+// ?callback=xx&a=1&b=2
+http.createServer((req, res) => {
+  if (req.url.includes('/api')) {
+    let json = {
+      code: 0
+    }
+    // ?callback=xx&a=1&b=2
+    qs.parse(req.url.split('?')[1]);
+    // {callback: 'xxx', a: 1, b: 2}
+    let fn = qs.parse(req.url.split('?')[1]).callback
+    res.end(`${fn}(${JSON.stringify(json)})`)
+  } else {
+    res.end('')
+  }
+})
+.listen(8088, () => {
+  console.log(8088);
+})
