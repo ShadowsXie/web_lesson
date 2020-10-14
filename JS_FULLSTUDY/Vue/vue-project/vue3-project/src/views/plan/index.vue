2fd0d516ebdeee2fd32527861b7027f6638f5192
ShadowsXie <1084827833@qq.com>
2020-10-14 20:08:54 +0800

    vue3-project

---
+++ JS_FULLSTUDY/Vue/vue-project/vue3-project/src/views/plan/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <create @handlePlan="handlePlan"></create>
+  </div>
+</template>
+
+<script>
+import Create from './create'
+export default {
+  components: {
+    Create
+  },
+  setup() {
+    const handlePlan = (plan) => {
+      
+    }
+
+    return {
+      handlePlan
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
