ac1b1804b7c2d2a0ba29310f3b6d66c0857fb051
ShadowsXie <1084827833@qq.com>
2020-10-23 19:58:56 +0800

    myKoa

--- JS_FULLSTUDY/mykoa/index.js
+++ JS_FULLSTUDY/mykoa/index.js
@@ -13,12 +13,35 @@
 
 // app.listen(3000)
 
-
 let Koa = require('./lib/application')
 let app = new Koa()
 
-app.use((req, res) => {
-  res.end('hello world')
+// app.use((ctx) => {
+//   console.log(ctx.req.url);
+//   console.log(ctx.request.req.url);
+//   console.log(ctx.response.req.url);
+//   console.log(ctx.request.url);
+//   console.log(ctx.request.path);
+//   console.log(ctx.url);
+//   console.log(ctx.path);
+// })
+
+app.use((ctx, next) => {
+  console.log(1);
+  next()
+  console.log(2);
+})
+
+app.use((ctx, next) => {
+  console.log(3);
+  next()
+  console.log(4);
+})
+
+app.use((ctx, next) => {
+  console.log(5);
+  next()
+  console.log(6);
 })
 
 app.listen(3000)
--- JS_FULLSTUDY/mykoa/lib/context.js
+++ JS_FULLSTUDY/mykoa/lib/context.js
@@ -1,4 +1,19 @@
 let proto = {}
 
+function defineGetter(prop, name) { // prop代表对象，namedai'bao+
+  proto.__defineGetter__(name, function() {  //每个对象都有__defineGetter__
+    return this[prop][name] // url == this.request.url
+  })
+}
+function defineSetter(prop, name) {
+  proto.__defineSetter__(name, function(val) {  //每个对象都有__defineGetter__
+    return this[prop][name] = val // url == this.request.url
+  })
+}
+
+defineGetter('request', 'url')
+defineGetter('request', 'path')
+defineGetter('response', 'body')
+defineSetter('response', 'body')
 
 module.exports = proto
