b3f7d1e3faef4026671a3d05fb0780996833fa5f
ShadowsXie <1084827833@qq.com>
2020-11-03 12:10:30 +0800

    vue=douban

---
+++ JS_FULLSTUDY/Vue/vue3.0/vue-douban/src/components/Movie.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="movie-box">
+    <van-image class="cover-image" radius="5px" :src="movie.image">
+      <template v-slot:error>
+        <img class="cover-image" src="https://img1.doubanio.com/view/photo/s_ratio_poster/public/p2623301908.webp" alt="">
+      </template>
+    </van-image>
+    <div class="title">{{movie.title}}</div>
+    <Star :rating="movie.rating"></Star>
+  </div>
+</template>
+
+<script>
+import { Image } from 'vant';
+import Star from './Star.vue'
+export default {
+  components: {
+    Star,
+    [Image.name]: Image
+  },
+  props:['movie']
+}
+</script>
+
+<style scoped>
+  .movie-box{
+        display: flex;
+        flex-direction: column;
+        width: 30%;
+        height: 205px;
+        margin-left: 10px;
+    }
+    .cover-image{
+        width: 100px;
+        height: 145px;
+    }
+    .title{
+        margin-top: 5px;
+        margin-bottom: 5px;
+        font-size: 15px;
+        text-align: left;
+        font-weight: bold;
+    }
+</style>
---
+++ JS_FULLSTUDY/Vue/vue3.0/vue-douban/src/views/Home.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="home">
+    <v-header title="豆瓣电影评分" :leftArrow="false"></v-header>
+    <movie-section :section="test"></movie-section>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Header from '@/components/Header.vue'
+import MovieSection from '@/components/MovieSection.vue'
+import { ref } from 'vue'
+export default {
+  name: 'Home',
+  components: {
+    'v-header': Header,
+    MovieSection
+  },
+  setup() {
+    const test = ref({
+      movies: [
+        {image:'https://img1.doubanio.com/view/photo/s_ratio_poster/public/p2623301908.webp', title:'金刚川', rating: { stars: [1, 1, 1, 0, 0], score: 6.5}},
+        {image:'https://img1.doubanio.com/view/photo/s_ratio_poster/public/p2623301908.webp', title:'金刚川', rating: { stars: [1, 1, 1, 0, 0], score: 6.5}},
+        {image:'https://img1.doubanio.com/view/photo/s_ratio_poster/public/p2623301908.webp', title:'金刚川', rating: { stars: [1, 1, 1, 0, 0], score: 6.5}},
+        {image:'https://img1.doubanio.com/view/photo/s_ratio_poster/public/p2623301908.webp', title:'金刚川', rating: { stars: [1, 1, 1, 0, 0], score: 6.5}},
+        {image:'https://img1.doubanio.com/view/photo/s_ratio_poster/public/p2623301908.webp', title:'金刚川', rating: { stars: [1, 1, 1, 0, 0], score: 6.5}},
+        {image:'https://img1.doubanio.com/view/photo/s_ratio_poster/public/p2623301908.webp', title:'金刚川', rating: { stars: [1, 1, 1, 0, 0], score: 6.5}}
+      ]
+    })
+
+    return {
+      test
+    }
+  }
+}
+</script>
