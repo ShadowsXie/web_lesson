9526d761306b5101d9073bd4a37b33138b861592
ShadowsXie <1084827833@qq.com>
2020-11-23 18:01:31 +0800

    原型

---
+++ JS_FULLSTUDY/interview/原型/1.js
@@ -0,0 +1,28 @@
+function Foo(){
+  getName = function(){
+      console.log(1)
+  }
+  return this;
+}
+Foo.getName = function(){
+  console.log(2)
+}
+Foo.prototype.getName = function(){
+  console.log(3)
+}
+var getName = function(){
+  console.log(4)
+}
+function getName(){
+  console.log(5)
+}
+// ouput:
+Foo.getName();  // 2
+getName();      // 4
+Foo().getName();  // 1
+getName();         // 1
+
+// 运算符优先级
+new Foo.getName();  // 2
+new Foo().getName(); // 3
+new new Foo().getName(); // 3
