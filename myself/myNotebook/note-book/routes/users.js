1e395a4b8169489077d4b65aef589c448ca7aedb
ShadowsXie <1084827833@qq.com>
2021-02-17 21:09:04 +0800

    myself

---
+++ myself/myNotebook/note-book/routes/users.js
@@ -0,0 +1,20 @@
+const router = require('koa-router')()
+const userService = require('../controllers/mySqlConfig')
+
+router.prefix('/users')
+
+router.get('/', function (ctx, next) {
+  ctx.body = 'this is a users response!'
+})
+
+router.get('/bar', function (ctx, next) {
+  ctx.body = 'this is a users/bar response'
+})
+
+router.get('/all', async(ctx, next) => {
+  await userService.getAllUsers().then((res) => {
+    ctx.body = res
+  })
+})
+
+module.exports = router
