7e10e84e642012425061825855d6c6da06da973c
ShadowsXie <1084827833@qq.com>
2021-02-22 22:47:25 +0800

    myself

--- myself/debounce.js
+++ myself/debounce.js
@@ -1,11 +1,21 @@
+// function debounce(fn, delay) {
+//   let timer = null
+//   return function () {
+//     clearTimeout(timer)
+//     timer = setTimeout(() => {
+//       fn.apply(this, arguments)
+//     }, delay)
+//   }
+// }
+
+// debounce只有第一次会执行，返回一个函数后，再执行的话只是执行这个返回的函数，所以timer = null 不会执行
+
 function debounce(fn, delay) {
   let timer = null
-  return function () {
+  return function() {
     clearTimeout(timer)
-    timer = setTimeout(() => {
+    let timer = setTimeout(() => {
       fn.apply(this, arguments)
     }, delay)
   }
 }
-
-// debounce只有第一次会执行，返回一个函数后，再执行的话只是执行这个返回的函数，所以timer = null 不会执行
