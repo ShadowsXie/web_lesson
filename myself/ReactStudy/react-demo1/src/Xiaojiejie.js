a9c2dda251da6aecd10f09c5c82bfc1820c2ac27
ShadowsXie <1084827833@qq.com>
2021-03-13 15:27:11 +0800

    react-demo

--- myself/ReactStudy/react-demo1/src/Xiaojiejie.js
+++ myself/ReactStudy/react-demo1/src/Xiaojiejie.js
@@ -1,8 +1,12 @@
 import React, { Component, Fragment } from 'react'
 import './style.css'
+// import axios from 'axios'
 import XiaojiejieItem from './XiaojiejieItem'
+import Boss from './Boss'
+import {CSSTransition , TransitionGroup} from 'react-transition-group'
 
 class Xiaojiejie extends Component {
+  // 在某一时刻，可以自动执行的函数（生命周期）
   constructor(props) {
     super(props)
     this.state = {
@@ -11,35 +15,85 @@ class Xiaojiejie extends Component {
     }
   }
 
+  // UNSAFE_componentWillMount() {
+  //   console.log(1);
+  // }
+
+  // componentDidMount() {
+  //   console.log(2);
+  // }
+
+  // shouldComponentUpdate() {
+  //   console.log('shouldComponentUpdate');
+  //   return true
+  // }
+
+  // UNSAFE_componentWillUpdate() {
+  //   console.log('UNSAFE_componentWillUpdate');
+  // }
+
+  // componentDidUpdate() {
+  //   console.log('componentDidUpdate');
+  // }
+
+  // componentDidMount() {
+  //   axios.get('https://web-api.juejin.im/v3/web/wbbr/bgeda')
+  //   .then((res) => {
+  //     console.log('请求成功' + JSON.stringify(res));
+  //   })
+  //   .catch((error) => {
+  //     console.log('请求失败' + error);
+  //   })
+  // }
+
   render() {
+    // console.log('render');
     return (
       <Fragment>
         <div>
           {/* 注释的正确写法 */}
           <label htmlFor = 'jspang'>增加服务：</label>
-          <input id = 'jspang' className = 'input' value = {this.state.inputValue} onChange = {this.inputChange.bind(this)}/> 
+          <input 
+            id = 'jspang' 
+            className = 'input' 
+            value = {this.state.inputValue} 
+            onChange = {this.inputChange.bind(this)}
+            ref = {(input) => {this.input = input}}
+          /> 
           <button onClick = {this.addList.bind(this)}>增加服务</button>
         </div>
-        <ul>
-          {
-            this.state.list.map((item, index) => {
-              return (
-                // <li 
-                //   key = {index + item}
-                //   onClick = {this.deleteItem.bind(this, index)}
-                //   dangerouslySetInnerHTML = {{__html:item}}
-                // >
-                // </li>
-                <XiaojiejieItem 
-                  key = {index + item} 
-                  content = {item}
-                  index = {index}
-                  deleteItem = {this.deleteItem.bind(this)}
-                />
-              )
-            })
-          }
+        <ul ref = {(ul) => {this.ul = ul}}>
+          <TransitionGroup>
+            {
+              this.state.list.map((item, index) => {
+                return (
+                  // <li 
+                  //   key = {index + item}
+                  //   onClick = {this.deleteItem.bind(this, index)}
+                  //   dangerouslySetInnerHTML = {{__html:item}}
+                  // >
+                  // </li>
+                  <CSSTransition
+                    timeout={1000}
+                    classNames='boss-text'
+                    unmountOnExit
+                    appear={true}
+                    key={index+item} 
+                  >
+                    <XiaojiejieItem 
+                      key = {index + item} 
+                      content = {item}
+                      index = {index}
+                      // list = {this.state.list}
+                      deleteItem = {this.deleteItem.bind(this)}
+                    />
+                  </CSSTransition>
+                )
+              })
+            }
+          </TransitionGroup>
         </ul>
+        <Boss />
       </Fragment>
     )
   }
@@ -47,7 +101,8 @@ class Xiaojiejie extends Component {
   inputChange(e) {
     // console.log(e.target.value);
     this.setState({
-      inputValue: e.target.value
+      // inputValue: e.target.value
+      inputValue: this.input.value
     })
   }
 
@@ -56,6 +111,8 @@ class Xiaojiejie extends Component {
     this.setState({
       list: [...this.state.list, this.state.inputValue],
       inputValue: ''
+    }, () => {
+      console.log(this.ul.querySelectorAll('li').length);
     })   
   }
 
--- myself/ReactStudy/react-demo1/src/XiaojiejieItem.js
+++ myself/ReactStudy/react-demo1/src/XiaojiejieItem.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import propTypes from 'prop-types'
 
 class XiaojiejieItem extends Component {
   constructor(props) {
@@ -6,15 +7,43 @@ class XiaojiejieItem extends Component {
     this.handleClick = this.handleClick.bind(this)
   }
   state = {  }
+  // 组件第一次存在于dom中，函数是不会被执行的
+  // 如果已经存在于dom，函数才会被执行
+  // UNSAFE_componentWillMount() {
+  //   console.log('UNSAFE_componentWillMount');
+  // }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    if (nextProps.content !== this.props.content) {
+      return true
+    } else {
+      return false
+    }
+  }
   render() { 
+    console.log('child-render');
     return (  
-      <li onClick = {this.handleClick}>{this.props.content}</li>
+      <li onClick = {this.handleClick}>
+        {this.props.avname}为你服务-{this.props.content}
+      </li>
     );
   }
 
   handleClick() {
+    // this.props.list = []
     this.props.deleteItem(this.props.index)
   }
+}
+
+XiaojiejieItem.propTypes = {
+  avname: propTypes.string.isRequired,
+  content: propTypes.string,
+  index: propTypes.number,
+  deleteItem: propTypes.func
+}
+
+XiaojiejieItem.defaultProps = {
+  avname: '蜗牛'
 }
  
 export default XiaojiejieItem;
